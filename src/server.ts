dea603c2557a9f83e998d5b61943881069f83f99
Sushant Lama <sushantlama732@gmail.com>
2025-12-15 22:02:48 +0545

    chnage the listen to everywhere with port change

--- src/server.ts
+++ src/server.ts
@@ -73,7 +73,7 @@ app.delete("/items/:id", async (req: Request, res: Response) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || "5000", 10);
 
 app.get("/", async (req: Request, res: Response) => {
     res.send("API is running....");
@@ -81,6 +81,9 @@ app.get("/", async (req: Request, res: Response) => {
 mongoose.connect(process.env.MONGO_URI || "")
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
+    console.log(`Server is accessible at http://localhost:${PORT}`);
+
   })
   .catch(err => console.error("MongoDB connection error:", err));
